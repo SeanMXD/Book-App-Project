5427d68b85b02c2f06f3339445858a6cf5bc8fe7
Sean Combs <seanmxd@gmail.com>
2020-04-09 20:21:04 -0400

    Created basic version

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,23 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor() {
+    this.decrementSection = this.decrementSection.bind(this);
+    this.incrementSection = this.incrementSection.bind(this);
+    this.setSection = this.setSection.bind(this);
+  }
 
+  section: number = 0;
+
+  decrementSection() {
+    if(this.section>0)
+      this.section--;
+  }
+  incrementSection() {
+    if(this.section < 5)
+      this.section++;
+  }
+  setSection(section: number) {
+    this.section = section;
+  }
 }
